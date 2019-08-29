0cb860328f9e96b6e32330c822fefe3e210e7072
Ryan Lopopolo <rjl@hyperbo.la>
2019-08-28 22:53:18 -0700

    Add project-infrastructure specific area labels

--- github/modules/repos/project-infrastructure/main.tf
+++ github/modules/repos/project-infrastructure/main.tf
@@ -25,7 +25,43 @@ resource "github_repository" "this" {
   }
 }
 
+module "colors" {
+  source = "../../colors"
+}
+
 module "labels" {
   source     = "../../labels/common"
   repository = local.repository
+}
+
+resource "github_issue_label" "area_aws" {
+  repository = local.repository
+  name = "A-aws"
+  description = "Area: AWS infrastructure as code."
+
+  color = module.colors.area
+}
+
+resource "github_issue_label" "area_dns" {
+  repository = local.repository
+  name = "A-dns"
+  description = "Area: DNS configuration."
+
+  color = module.colors.area
+}
+
+resource "github_issue_label" "area_github" {
+  repository = local.repository
+  name = "A-github"
+  description = "Area: GitHub infrastructure as code."
+
+  color = module.colors.area
+}
+
+resource "github_issue_label" "area_google_domains" {
+  repository = local.repository
+  name = "A-google-domains"
+  description = "Area: Google Domains infrastructure as code."
+
+  color = module.colors.area
 }
