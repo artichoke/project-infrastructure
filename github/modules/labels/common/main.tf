5f17914fcf95ff1fcc2fe2735486c66412684cc5
Ryan Lopopolo <rjl@hyperbo.la>
2019-08-28 22:32:33 -0700

    Add status and call for participation labels

--- github/modules/labels/common/main.tf
+++ github/modules/labels/common/main.tf
@@ -59,3 +59,91 @@ resource "github_issue_label" "category_question" {
 
   color = module.colors.category
 }
+
+resource "github_issue_label" "call_for_participation_easy" {
+  repository = var.repository
+  name = "E-easy"
+  description = "Call for participation: Experience needed to fix: Easy / not much."
+
+  color = module.colors.call_for_participation
+}
+
+resource "github_issue_label" "call_for_participation_hard" {
+  repository = var.repository
+  name = "E-hard"
+  description = "Call for participation: Experience needed to fix: Hard / a lot."
+
+  color = module.colors.call_for_participation
+}
+
+resource "github_issue_label" "call_for_participation_help_wanted" {
+  repository = var.repository
+  name = "E-help-wanted"
+  description = "Call for participation: Help is requested to fix this issue."
+
+  color = module.colors.call_for_participation
+}
+
+resource "github_issue_label" "call_for_participation_medium" {
+  repository = var.repository
+  name = "E-medium"
+  description = "Call for participation: Experience needed to fix: Medium / intermediate."
+
+  color = module.colors.call_for_participation
+}
+
+resource "github_issue_label" "call_for_participation_needs_test" {
+  repository = var.repository
+  name = "E-needs-test"
+  description = "Call for participation: writing correctness tests."
+
+  color = module.colors.call_for_participation
+}
+
+resource "github_issue_label" "status_blocked" {
+  repository = var.repository
+  name = "S-blocked"
+  description = "Status: marked as blocked ❌ on something else such as other implementation work."
+
+  color = module.colors.status
+}
+
+resource "github_issue_label" "status_do_not_merge" {
+  repository = var.repository
+  name = "S-do-not-merge"
+  description = "Status: This pull request should not be merged."
+
+  color = module.colors.status
+}
+
+resource "github_issue_label" "status_duplicate" {
+  repository = var.repository
+  name = "S-duplicate"
+  description = "Status: This issue or pull request already exists."
+
+  color = module.colors.status
+}
+
+resource "github_issue_label" "status_invalid" {
+  repository = var.repository
+  name = "S-invalid"
+  description = "Status: This issue or pull request is not well-formed."
+
+  color = module.colors.status
+}
+
+resource "github_issue_label" "status_wip" {
+  repository = var.repository
+  name = "S-wip"
+  description = "Status: This pull request is a work in progress."
+
+  color = module.colors.status
+}
+
+resource "github_issue_label" "status_wontfix" {
+  repository = var.repository
+  name = "S-wontfix"
+  description = "Status: This issue or pull request will not be worked on."
+
+  color = module.colors.status
+}
