e2ccd767c92a4d63d7fc9b3ba7e6851ff012bf0f
Ryan Lopopolo <rjl@hyperbo.la>
2021-08-02 18:49:38 -0700

    Manage Docker Hub secrets for docker-artichoke-nightly workflows in terraform
    Using scoped organization secrets.
    
    Fixes #63.

--- github/organization-secrets.tf
+++ github/organization-secrets.tf
@@ -12,6 +12,16 @@ data "terraform_remote_state" "aws" {
   }
 }
 
+variable "dockerhub_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "dockerhub_user" {
+  type      = string
+  sensitive = true
+}
+
 resource "github_actions_organization_secret" "terraform_aws_access_key" {
   secret_name     = "TF_AWS_ACCESS_KEY"
   visibility      = "selected"
@@ -26,4 +36,20 @@ resource "github_actions_organization_secret" "terraform_aws_secret_key" {
   plaintext_value = data.terraform_remote_state.aws.outputs.github_actions_iam_secret_keys["project-infrastructure"]
 
   selected_repository_ids = [github_repository.project_infrastructure.repo_id]
+}
+
+resource "github_actions_organization_secret" "dockerhub_token" {
+  secret_name     = "DOCKERHUB_TOKEN"
+  visibility      = "selected"
+  plaintext_value = var.dockerhub_token
+
+  selected_repository_ids = [github_repository.docker_artichoke_nightly.repo_id]
+}
+
+resource "github_actions_organization_secret" "dockerhub_user" {
+  secret_name     = "DOCKERHUB_USER"
+  visibility      = "selected"
+  plaintext_value = var.dockerhub_user
+
+  selected_repository_ids = [github_repository.docker_artichoke_nightly.repo_id]
 }
