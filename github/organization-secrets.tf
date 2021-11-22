59440bcd791cb8d2c9771d3063dccaddeca055f4
Ryan Lopopolo <rjl@hyperbo.la>
2021-11-21 22:11:16 -0800

    Remove AWS access keys from GitHub organization secrets
    Followup to #155.

--- github/organization-secrets.tf
+++ github/organization-secrets.tf
@@ -1,15 +1,3 @@
-data "terraform_remote_state" "aws" {
-  backend = "s3"
-
-  config = {
-    bucket         = "artichoke-forge-project-infrastructure-terraform-state"
-    region         = "us-west-2"
-    key            = "aws/terraform.tfstate"
-    encrypt        = true
-    dynamodb_table = "terraform_statelock"
-  }
-}
-
 variable "dockerhub_token" {
   type      = string
   sensitive = true
@@ -18,22 +6,6 @@ variable "dockerhub_token" {
 variable "dockerhub_user" {
   type      = string
   sensitive = true
-}
-
-resource "github_actions_organization_secret" "terraform_aws_access_key" {
-  secret_name     = "TF_AWS_ACCESS_KEY"
-  visibility      = "selected"
-  plaintext_value = data.terraform_remote_state.aws.outputs.github_actions_iam_access_ids["project-infrastructure"]
-
-  selected_repository_ids = [github_repository.project_infrastructure.repo_id]
-}
-
-resource "github_actions_organization_secret" "terraform_aws_secret_key" {
-  secret_name     = "TF_AWS_SECRET_KEY"
-  visibility      = "selected"
-  plaintext_value = data.terraform_remote_state.aws.outputs.github_actions_iam_secret_keys["project-infrastructure"]
-
-  selected_repository_ids = [github_repository.project_infrastructure.repo_id]
 }
 
 resource "github_actions_organization_secret" "dockerhub_token" {
