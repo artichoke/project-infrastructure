eabef0d1b54d8eb081414c65c4e482c56e2f7cc4
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-11 21:52:17 -0800

    Explicitly use integrations/github in github-discord-webhook module

--- modules/github-discord-webhook/main.tf
+++ modules/github-discord-webhook/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.20"
+    }
+  }
+}
+
 resource "github_organization_webhook" "discord" {
   configuration {
     url          = "https://discordapp.com/api/webhooks/${var.webhook_id}/${var.webhook_token}/github"
