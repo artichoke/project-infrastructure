7f26869a9ed6961983b7ae49002cdc6cf4507ab7
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-12 16:09:15 -0800

    Add a terraform module for pushing a file to a new branch in a GitHub repository

---
+++ modules/update-github-repository-file/outputs.tf
@@ -0,0 +1,10 @@
+output "branch_href" {
+  description = "Link to the newly-created branch"
+  value = join("/", [
+    "https://github.com/",
+    var.organization,
+    var.repository,
+    "tree",
+    replace(github_branch.pr_target.ref, "refs/heads/", ""),
+  ])
+}
