8a41e4bced17a36a5758f225a3a15d1e38dcc1d1
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-13 08:18:07 -0800

    Use new team modules in github-org-artichoke environment

--- github-org-artichoke/versions.tf
+++ github-org-artichoke/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.12"
+      version = "~> 4.20"
     }
   }
   required_version = "~> 1.0"
