a44f0fbf1980f40aa3f86b0f8668d96605fd4542
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-15 18:32:07 -0800

    Migrate artichoke GitHub org webhook to tf module, add security events webhook

---
+++ github-org-artichoke/variables.tf
@@ -0,0 +1,27 @@
+variable "github_token" {
+  description = "GitHub Personal Access Token (PAT) with full access"
+  type        = string
+  sensitive   = true
+}
+
+variable "discord_git_events_webhook_id" {
+  description = "Discord webhook id for #gitlog"
+  type        = string
+}
+
+variable "discord_git_events_webhook_token" {
+  description = "Discord webhook secret token for #gitlog"
+  type        = string
+  sensitive   = true
+}
+
+variable "discord_security_events_webhook_id" {
+  description = "Discord webhook id for #security-events"
+  type        = string
+}
+
+variable "discord_security_events_webhook_token" {
+  description = "Discord webhook secret token for #security-events"
+  type        = string
+  sensitive   = true
+}
