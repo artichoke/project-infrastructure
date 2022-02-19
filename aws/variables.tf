2a47e5aad02b7cdf9fe35fb8a83c99beeb609bf2
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-18 19:15:45 -0800

    Clarify the difference between project and environment in aws environment provider tags

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,4 +1,10 @@
 variable "environment" {
+  description = "Terraform environment bulkhead"
+  default     = "aws"
+  type        = string
+}
+
+variable "project" {
   description = "Workspace name"
   default     = "artichoke-forge"
   type        = string
