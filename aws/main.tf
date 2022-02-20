bbaa2b43b706d0e592f2bfa660fe0bd685a24a9f
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-20 12:51:48 -0800

    Provision GitHub backups infra for project-infrastructure repo

--- aws/main.tf
+++ aws/main.tf
@@ -8,6 +8,12 @@ terraform {
   }
 }
 
+locals {
+  backed_up_repositories = [
+    "project-infrastructure",
+  ]
+}
+
 module "github_actions_oidc_provider" {
   source = "../modules/github-actions-oidc-provider"
 }
@@ -20,4 +26,28 @@ module "github_actions_project_infrastructure_assume_role" {
   github_repository        = "project-infrastructure"
 
   s3_bucket_name = "artichoke-forge-project-infrastructure-terraform-state"
+}
+
+module "repo_backups_access_logs" {
+  source = "../modules/access-logs-s3-bucket"
+
+  bucket = "artichoke-forge-github-backups-logs"
+}
+
+module "repo_backups" {
+  source = "../modules/private-s3-bucket"
+
+  bucket             = "artichoke-forge-github-backups"
+  access_logs_bucket = module.repo_backups_access_logs.name
+}
+
+module "github_actions_s3_backups_assume_role" {
+  source   = "../modules/github-actions-s3-repo-backup-access"
+  for_each = toset(local.backed_up_repositories)
+
+  github_oidc_provider_arn = module.github_actions_oidc_provider.arn
+  github_organization      = "artichoke"
+  github_repository        = each.value
+
+  s3_bucket_name = module.repo_backups.name
 }
--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,4 +1,9 @@
 output "github_actions_project_infrastructure_assume_role_arn" {
-  value       = module.github_actions_project_infrastructure_assume_role.role_arn
   description = "This is the ARN of the role that can be assumed in GitHub Actions to access AWS resources"
+  value       = module.github_actions_project_infrastructure_assume_role.role_arn
+}
+
+output "github_actions_s3_backup_assume_role_arn" {
+  description = "This is the ARN of the role that can be assumed in GitHub Actions to access AWS resources"
+  value       = {for repo, m in module.github_actions_s3_backups_assume_role : repo => m.role_arn}
 }
