1870d01d401052065ad977cabe6211a8212fd5f1
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-20 23:33:00 -0800

    Add `s3-backup.yaml` GitHub repo orchestration with terraform
    Add a new instance of the `update-github-repository-file` module to
    place a GitHub Actions workflow called `s3-backup.yaml` into Artichoke
    repositories.
    
    This code creates a workflow that will generate a backup of the
    repository, issues, comments, pulls, and releases and upload them to S3
    as tarballs and zips. Releases are uploaded directly and not archived
    and compressed.
    
    The workflow is templated with the AWS IAM role and S3 bucket created in
    the `aws` environment for backups. Backups are generated on a cron once
    a month. The day for the cron is randomly chosen between 1 and 25 to
    give some skew and make sure we don't hit GitHub rate limits.
    
    The workflow uses the `github-backup` Python package to execute the
    backup using a GitHub access token generated by the GitHub Actions
    workflow. https://github.com/josegonzalez/python-github-backup
    
    This backup code is inspired by this article, but using GitHub Actions
    for cron and access keys instead:
    
    - https://www.marcolancini.it/2021/blog-github-backups-with-ecs/
    
    The workflow uses its workflow permissions to also acquire a set of AWS
    credentials using OpenID Connect federation to make the uploads to S3.
    See:
    
    - #229
    - #230
    
    The resulting backups are uploaded to a repository-specific prefix and
    are laid out like this:
    
        s3://artichoke-forge-github-backups-us-west-2/project-infrastructure/archives/tarballs/project-infrastructure-2022-02-20.tar.gz
        s3://artichoke-forge-github-backups-us-west-2/project-infrastructure/archives/zips/project-infrastructure-2022-02-20.zip
        s3://artichoke-forge-github-backups-us-west-2/project-infrastructure/releases/<release name 1>
        s3://artichoke-forge-github-backups-us-west-2/project-infrastructure/releases/<release name 2>
    
    Backups are ecrypted with AWES256 S3-SSE and S3-managed keys.
    
    This bit of terraform code has been tested and deployed in:
    
    - #232
    - https://github.com/artichoke/project-infrastructure/pull/232#issuecomment-1047019970

--- github-org-artichoke/outputs.tf
+++ github-org-artichoke/outputs.tf
@@ -23,6 +23,11 @@ output "audit_workflow_node_ruby_rust_branches" {
   value       = join("\n", concat(length(module.audit_workflow_node_ruby_rust) == 0 ? [] : ["## Branches", ""], formatlist("- %s", [for repo, audit_workflow in module.audit_workflow_node_ruby_rust : audit_workflow.branch_href])))
 }
 
+output "s3_backup_workflow_branches" {
+  description = "Links to GitHub with changes to the `s3-backup` workflow"
+  value       = join("\n", concat(length(module.s3_backup_workflow) == 0 ? [] : ["## Branches", ""], formatlist("- %s", [for repo, backup_workflow in module.s3_backup_workflow : backup_workflow.branch_href])))
+}
+
 output "ruby_version_branches" {
   description = "A list of links to branches on GitHub with changes to .ruby-version"
   value       = join("\n", concat(length(module.ruby_version) == 0 ? [] : ["## Branches", ""], formatlist("- %s", [for repo, ruby_version in module.ruby_version : ruby_version.branch_href])))
---
+++ github-org-artichoke/repository_file_github_actions_s3_backup_workflow.tf
@@ -0,0 +1,48 @@
+locals {
+  // Set `force_bump_s3_backup` to true to create branches for PRs that update
+  // the S3 backup workflow organization-wide.
+
+  force_bump_s3_backup = false
+  s3_backup_repos = [
+    "project-infrastructure", // https://github.com/artichoke/project-infrastructure
+  ]
+}
+
+data "terraform_remote_state" "aws" {
+  backend = "s3"
+
+  config = {
+    bucket         = "artichoke-forge-project-infrastructure-terraform-state"
+    region         = "us-west-2"
+    key            = "aws/terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "terraform_statelock"
+  }
+}
+
+resource "random_integer" "cron_day_of_month" {
+  for_each = toset(local.s3_backup_repos)
+
+  min = 1
+  max = 25
+}
+
+module "s3_backup_workflow" {
+  source   = "../modules/update-github-repository-file"
+  for_each = toset(local.force_bump_s3_backup ? local.s3_backup_repos : [])
+
+  organization = "artichoke"
+  repository   = each.value
+  base_branch  = "trunk"
+  file_path    = ".github/workflows/s3-backup.yaml"
+  file_contents = templatefile(
+    "${path.module}/templates/s3-backup.yaml",
+    {
+      cron_day_of_month            = random_integer.cron_day_of_month[each.value].result,
+      s3_bucket                    = data.terraform_remote_state.aws.outputs.github_actions_s3_backup_s3_bucket,
+      backup_role_arn              = data.terraform_remote_state.aws.outputs.github_actions_s3_backup_assume_role_arn[each.value],
+      github_repository            = each.value,
+      python_github_backup_version = "0.40.1", // https://pypi.org/project/github-backup/
+    }
+  )
+}
