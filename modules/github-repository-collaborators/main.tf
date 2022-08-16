e0765e68542a12cf701d7d5b08c43da7d38c3041
Ryan Lopopolo <rjl@hyperbo.la>
2022-08-16 16:52:35 -0700

    terraform fmt

--- modules/github-repository-collaborators/main.tf
+++ modules/github-repository-collaborators/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "github_repository_collaborator" "collaborators" {
-  for_each = {for config in var.collaborators : config.user => config.role}
+  for_each = { for config in var.collaborators : config.user => config.role }
 
   repository = var.repository
   username   = each.key
