c827b9c75d0ec85cd6f7ab4fa7fcab3470558550
Ryan Lopopolo <rjl@hyperbo.la>
2022-08-16 18:02:57 -0700

    Add code coverage bucket, roles, and cloudfront website

--- aws/main.tf
+++ aws/main.tf
@@ -12,6 +12,10 @@ locals {
   backed_up_repositories = [
     "project-infrastructure",
   ]
+
+  code_coverage_repositories = [
+    "strftime-ruby",
+  ]
 }
 
 module "github_actions_oidc_provider" {
@@ -57,4 +61,24 @@ module "github_actions_s3_backups_assume_role" {
   github_repository        = each.value
 
   s3_bucket_name = module.repo_backups.name
+}
+
+module "code_coverage" {
+  source = "../modules/s3-bucket-website"
+
+  bucket             = "artichoke-forge-code-coverage-${var.region}"
+  access_logs_bucket = module.forge_access_logs.name
+
+  domains = ["codecov.artichokeruby.org"]
+}
+
+module "github_actions_code_coverage_assume_role" {
+  source   = "../modules/github-actions-s3-repo-backup-access"
+  for_each = toset(local.code_coverage_repositories)
+
+  github_oidc_provider_arn = module.github_actions_oidc_provider.arn
+  github_organization      = "artichoke"
+  github_repository        = each.value
+
+  s3_bucket_name = module.code_coverage.name
 }
