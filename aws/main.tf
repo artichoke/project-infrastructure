444efbee8ba3b81bf72390550aefa58dad87ba42
Ryan Lopopolo <rjl@hyperbo.la>
2022-08-17 17:13:54 -0700

    Add favicons to codecov S3 bucket

--- aws/main.tf
+++ aws/main.tf
@@ -98,3 +98,25 @@ resource "aws_s3_object" "code_coverage_index_html" {
 
   server_side_encryption = "AES256"
 }
+
+resource "aws_s3_object" "code_coverage_favicon" {
+  bucket = module.code_coverage.name
+  key    = "favicon.png"
+  source = "${path.module}/favicon-32x32.png"
+
+  etag         = filemd5("${path.module}/favicon-32x32.png")
+  content_type = "image/png"
+
+  server_side_encryption = "AES256"
+}
+
+resource "aws_s3_object" "code_coverage_favicon_ico" {
+  bucket = module.code_coverage.name
+  key    = "favicon.ico"
+  source = "${path.module}/favicon.ico"
+
+  etag         = filemd5("${path.module}/favicon.ico")
+  content_type = "image/x-icon"
+
+  server_side_encryption = "AES256"
+}
