47a4a799a08ad1b9da6d892bced5866a65e2b246
Ryan Lopopolo <rjl@hyperbo.la>
2023-04-10 21:28:50 -0700

    Track 'random' terraform provider in lockfile and version constraints

--- github-org-artichoke/versions.tf
+++ github-org-artichoke/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 5.9"
