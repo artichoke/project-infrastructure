0f02a532cc92c556d70a0f0bdfc3e2862433abad
Ryan Lopopolo <rjl@hyperbo.la>
2023-04-10 18:43:08 -0700

    Update lower bound on GutHub provider to support managing GHA permissions

--- github-org-artichoke/versions.tf
+++ github-org-artichoke/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.3"
+      version = "~> 5.9"
     }
   }
   required_version = "~> 1.0"
